f655c7313028dbefed37600304a1ac0c0a67c78f
kgeoffroy <kirsan.geoffroy@etu.u-bordeaux.fr>
2021-11-17 15:37:02 +0100

    ajout CRUD pour column model

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { getAllUsers, getUserById, updateUser, removeUser, addUser } = require('../controllers/userController');
+const { getAllColumns, getColumnById, updateColumn, removeColumn, addColumn } = require('../controllers/columnController');
 
 router.get('/', function (req, res) {
     res.status(200).json({
@@ -8,6 +9,7 @@ router.get('/', function (req, res) {
     });
 });
 
+// USER //
 router.route('/users').get(async (req, res) => {
     let response = await getAllUsers();
     if (response.success == true) {
@@ -46,6 +48,52 @@ router.route('/updateUser/:id').put(async (req, res) => {
 
 router.route('/removeUser/:id').delete(async (req, res) => {
     let response = await removeUser(req.params.id);
+    if (response.success == true) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json(response);
+    }
+});
+
+// COLUMN // 
+router.route('/columns').get(async (req, res) => {
+    let response = await getAllColumns();
+    if (response.success == true) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json(response);
+    }
+});
+
+router.route('/column/:id').get(async (req, res) => {
+    let response = await getColumnById(req.params.id);
+    if (response.success == true) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json(response);
+    }
+});
+
+router.route('/addColumn/').post(async (req, res) => {
+    let response = await addColumn(req.body);
+    if (response.success == true) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json(response);
+    }
+});
+
+router.route('/updateColumn/:id').put(async (req, res) => {
+    let response = await updateColumn(req.params.id, req.body);
+    if (response.success == true) {
+        res.status(200).json(response);
+    } else {
+        res.status(404).json(response);
+    }
+});
+
+router.route('/removeColumn/:id').delete(async (req, res) => {
+    let response = await removeColumn(req.params.id);
     if (response.success == true) {
         res.status(200).json(response);
     } else {
