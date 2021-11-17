40b115cae75465ae43a95f5483035ab71e8637e9
valentinflegon <valfleg@gmail.com>
2021-11-18 00:31:15 +0100

    page accueil mise en forme

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,7 +1,3 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 
 export class UserService {
