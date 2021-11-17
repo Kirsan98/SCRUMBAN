b5f5c32b0deb50061822702c5658a6a6c741d2d1
valentinflegon <valfleg@gmail.com>
2021-11-17 21:54:56 +0100

    route pour accueil et projeccts ainsi qu'une route par defaut

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'accueil',component: AccueilComponent},
+  {path: 'projects',component: ProjectsComponent},
+  { path: '', pathMatch: 'full', redirectTo: 'accueil' },
+  { path: '**', redirectTo: 'accueil' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
