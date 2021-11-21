0e0e09dccb945291f5b54081cf7d5c067895959c
valentinflegon <valfleg@gmail.com>
2021-11-21 23:27:46 +0100

    delete message

--- frontend/src/app/projects/delete-project/delete-project.component.ts
+++ frontend/src/app/projects/delete-project/delete-project.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Project } from 'src/app/models/project.model';
 import { ProjectService } from 'src/app/services/project.service';
+
+
+//TODO 
+// checker si le input rentrer et le meme que le titre du projet
+// si oui alors on delete le projet 
+// si non alors ...
+
 
 @Component({
   selector: 'app-delete-project',
@@ -10,14 +18,18 @@ import { ProjectService } from 'src/app/services/project.service';
 })
 export class DeleteProjectComponent implements OnInit {
   public project!: Project;
+  public deleteProjectForm !: FormGroup;
+
 
   constructor(
+    private formBuilder: FormBuilder,
     private router: Router,
     private route: ActivatedRoute,
     private projectService: ProjectService
   ) { }
 
   ngOnInit(): void {
+ 
     this.route.params.subscribe(
       (params: Params) => {
         this.projectService.getProjectById(params.id).then(
