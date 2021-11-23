fcd8a5433895893c30403d743dfe5b79f87b40ab
Kirsan98 <saito-kik@hotmail.com>
2021-11-23 08:58:40 +0100

    angular version update, npm update, ng update

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,12 +5,12 @@ dotenv.config();
 
 async function connectDB() {
     try {
-        console.log("Opening connection")
+        console.log("Opening connection to the database")
         var mongoDB = 'mongodb+srv://cdp:BHdUDx2iU9gWcBOC@clusterscrumban.rsjdb.mongodb.net/scrumban_db?retryWrites=true&w=majority';
         mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-        console.log("connection successfull");
+        console.log("Connected successfull");
     } catch(err) { 
         mongoose.disconnect();
         console.error(err);
