25650599d2bc4f6ba2a9922882bbbda45ab24dc5
Kirsan98 <saito-kik@hotmail.com>
2021-12-07 09:38:27 +0100

    implement reactive form for signup page

--- frontend/src/app/auth/signup/signup.component.ts
+++ frontend/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import Validation from '../../utils/validation';
 
 @Component({
   selector: 'app-signup',
@@ -7,14 +8,64 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  signupForm!: FormGroup;
-  errorMessage!: string;
+  signupForm: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group(
+      {
+        firstname: ['', Validators.required],
+        lastname: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+          ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+    );
   }
 
-  onSignup() {}
+  get f(): { [key: string]: AbstractControl } {
+    return this.signupForm.controls;
+  }
 
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.signupForm.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.signupForm.value, null, 2));
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.signupForm.reset();
+  }
 }
