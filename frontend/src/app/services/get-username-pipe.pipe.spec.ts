81592dc7d12ff08424e6d1fb6b9e134dd48e4c6d
chtoudi <duplantier.louis@gmail.com>
2021-12-15 00:40:02 +0100

    clean fichier innutile

--- frontend/src/app/services/get-username-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GetUsernamePipe } from './get-username-pipe.pipe';
-
-describe('GetUsernamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new GetUsernamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
